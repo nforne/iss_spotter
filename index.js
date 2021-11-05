b906def918749efd3508b2ba652664df3ec6d010
nforne <mnforne@gmail.com>
2021-11-05 05:48:09 -0700

    add coordates fetcher

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -10,6 +10,15 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
+fetchCoordsByIP('64.229.22.233', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 /*
 // index.js
 
--- iss.js
+++ iss.js
@@ -8,13 +8,10 @@
  */
 const request = require('request');
 
-const gcl = 'https://api.ipify.org/?format=json'; // link to be fetched;
-
-
-
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-  request(gcl, (error, response, body) => {
+  const ltbf = 'https://api.ipify.org/?format=json'; // link to be fetched;
+  request(ltbf, (error, response, body) => {
     
     // inside the request callback ...
     // error can be set if invalid domain, user is offline, etc.
@@ -37,34 +34,37 @@ const fetchMyIP = function(callback) {
 
 };
 
-module.exports = { fetchMyIP };
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-/*
-// iss.js
-const request = require('request');
+const fetchCoordsByIP = (ip, callback) => {
+  const ltbf = `https://api.freegeoip.app/json/${ip}?apikey=8cf66000-3e31-11ec-9466-13e02b8af772`; // link to be fetched;
 
-
-//  * Makes a single API request to retrieve the user's IP address.
-//  * Input:
-//  *   - A callback (to pass back an error or the IP string)
-//  * Returns (via Callback):
-//  *   - An error, if any (nullable)
-//  *   - The IP address as a string (null if error). Example: "162.245.144.188"
- 
- const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
-
+  request(ltbf, (error, response, body) => {
+    
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
       return;
     }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+    // if we get here, all's well and we got the data
+    const data = JSON.parse(body);
+    let outPut = {};
+    outPut['latitude'] = data.latitude;
+    outPut['longitude'] = data.longitude;
+    callback(error, outPut);
+    
   });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP};
 
-*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
